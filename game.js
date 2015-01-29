60a1e4ce5ba2dc85d57381907dea6220ab0cf1d2
John M Davis Jr <jmdcodejunkie@gmail.com>
2015-01-29 15:09:59 -0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(800, 600,
+Phaser.AUTO, '',
+{ preload: _preload, create: _create, update: _update }
+);
+
+var _preload = function() {
+
+}
+
+var _create = function() {
+
+}
+
+var _update = function() {
+
+}
