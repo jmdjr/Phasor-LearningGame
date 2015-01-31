05eea5f595e9b3fe66e2a3fff17e07beb7edee53
John M Davis Jr <jmdcodejunkie@gmail.com>
2015-01-30 20:55:27 -0800

    small milestone
    debug text, two objects colliding... and some gravity... good first
    step.

--- javascripts/game-intro.js
+++ javascripts/game-intro.js
@@ -1,16 +1,44 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-intro', { preload: preload, create: create, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-intro', { preload: preload, create: create, render: render, update: update });
 
-var preload = function() {
+function preload() {
     game.load.image('ball', 'images/Ball.png');
+    game.load.image('paddle', 'images/Paddle.png');
 }
 
 var ball;
 var paddle;
 
-var create = function() {
+function create() {
+    game.stage.backgroundColor = '#0020FF';
     
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    ball = game.add.sprite(400, 200, 'ball');  
+    paddle = game.add.sprite(400, 400, 'paddle');
+    
+    //ball is 66x66
+    ball.anchor.set(0.5);
+    
+    //paddle is 800 x 66
+    paddle.anchor.set(0.5);
+    
+    game.physics.enable([ball, paddle], Phaser.Physics.ARCADE);
+    
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.y = 0.5;
+    ball.body.gravity.y = 500;
+    
+    paddle.body.collideWorldBounds = true;
 }
 
-var render = function() {
+function render() {
+    game.debug.inputInfo(10, 20);
+}
+
+function update() {
+    game.physics.arcade.collide(ball, paddle, collionHandler, null, this);
+}
+
+function collionHandler(a, b) {
     
 }
