a3acd01500b9ecf1a77a5d2318ee6110574c82e6
John M Davis Jr <jmdcodejunkie@gmail.com>
2015-01-30 19:46:13 -0800

    work on game

--- javascripts/game-intro.js
+++ javascripts/game-intro.js
@@ -1,5 +1,16 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-intro', { preload: preload, create: create, render: render });
 
 var preload = function() {
-    game.load.image('');
+    game.load.image('ball', 'images/Ball.png');
+}
+
+var ball;
+var paddle;
+
+var create = function() {
+    
+}
+
+var render = function() {
+    
 }
